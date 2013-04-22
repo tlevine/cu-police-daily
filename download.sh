f365e429f8523ecd46b4e5d5cda3c346faf438c1
Thomas Levine <occurrence@thomaslevine.com>
2013-04-22 08:30:41 -0400

    rm the one empty file

--- download.sh
+++ download.sh
@@ -2,6 +2,6 @@
 
 repid=$(curl http://dailycrimelog.cupolice.cornell.edu/ 2> /dev/null | grep -m1 RepID | sed 's/.*=\([^"]*\)".*$/\1/')
 while test -f  "downloads/$repid" || curl "http://dailycrimelog.cupolice.cornell.edu/?RepID=${repid}" > "downloads/$repid"; do
+  grep 'There is no report for the date selected' "downloads/${repid}" 1>/dev/null && rm  "downloads/${repid}" && break
   repid=$(($repid - 1))
-  sleep 1s
 done
