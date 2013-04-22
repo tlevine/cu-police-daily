c888bb21a3431ff83d144ce9444ba68ae2544b5c
Thomas Levine <occurrence@thomaslevine.com>
2013-04-22 07:40:34 -0400

    go

--- download.sh
+++ download.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 repid=$(curl http://dailycrimelog.cupolice.cornell.edu/ 2> /dev/null | grep -m1 RepID | sed 's/.*=\([^"]*\)".*$/\1/')
+while test -f  "downloads/$repid" || curl "http://dailycrimelog.cupolice.cornell.edu/?RepID=${repid}" > "downloads/$repid"; do
+  repid=$(($repid - 1))
+  sleep 1s
+done
